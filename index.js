f67db3c3129cb5ceddf08c87cd7556bc483a6491
Phoenix Deinert <phoenix.deinert@gmail.com>
2023-05-09 22:25:49 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ];
+  function superbowlWin(array) {
+    const result = array.find(obj => obj.result === "W");
+    return result ? result.year : undefined;
+  }
+  function writeCards(namesArray, event) {
+    const thankYouMessages = [];
+    for (let i = 0; i < namesArray.length; i++) {
+      thankYouMessages.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouMessages;
+  }
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+        // code your solution here
